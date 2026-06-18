0e70bc4182bd6ad7fd94badf62bea900711ede18
sarahjl1909 <sarah.jl1909@gmail.com>
2026-06-17 22:10:38 -0400

    feat: add endpoint get with id

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ banco_de_dados = []
 def read_task():
     return banco_de_dados
 
+@app.get("/tasks/{task_id}")
+def read_task_item(task_id: int):
+    
+    for task in banco_de_dados:
+        if task.id == task_id:
+            return task
+    return ["nao encontrado"]
+    
+    
+
 @app.post("/tasks/")
 async def create_task(task: Task):
     banco_de_dados.append(task)
